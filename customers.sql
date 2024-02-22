79ec97dc569b9112a981240b04485301bb3928ee
jonhaestefania <jonhatan.estefania@gmail.com>
2024-02-21 18:25:53 -0600

    adds sql script for create the first table customers

---
+++ customers.sql
@@ -0,0 +1,9 @@
+/*
+ Create table for stores customers
+ */
+CREATE TABLE customers(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    state VARCHAR(45)
+);
